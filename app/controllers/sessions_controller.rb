3950f1f807b48cd525e80a25803b18de0a3b3ee2
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-10-22 16:24:47 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,13 @@
 class SessionsController < ApplicationController
   def new
-
   end
 
   def create
-
+    if params[:name].empty?
+      redirect_to "new"
+    else
+      session[:name] = params[:name]
+    end
   end
 
   def destroy
